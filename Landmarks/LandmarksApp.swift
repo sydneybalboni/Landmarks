f18323090da70e0ca1a6fbc68e4878a8efc8efd2
Sydney Balboni <sydneybalboni@outlook.com>
2022-11-21 12:48:33 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sydney Balboni on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
